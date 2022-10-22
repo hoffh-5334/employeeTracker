fb51ee7e797c220a8add878cc97ac85c9f35c26e
Holly Hoffman <hoffh5334@gmail.com>
2022-10-21 20:38:03 -0500

    built .env.example. Added constraints and tables to schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,12 +5,15 @@ USE employees;
 
 CREATE TABLE department (
   -- insert the id and name columns here --
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   -- insert the title and salary columns here --
-  
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
   department_id INT UNSIGNED NOT NULL,
   INDEX dep_ind (department_id),
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
@@ -27,6 +30,7 @@ CREATE TABLE employee (
   -- references the id column of the role table, and performs a cascade delete when needed --
 
   -- constraint goes here --
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
   manager_id INT UNSIGNED,
   INDEX man_ind (manager_id),
 
@@ -34,4 +38,5 @@ CREATE TABLE employee (
   -- references the id column of the employee table, and just sets NULL on a delete action
   
   -- constraint goes here
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
