e0c3dc74f36312949b995fdd559dd8c0e4b539e7
Holly Hoffman <hoffh5334@gmail.com>
2022-10-21 20:56:54 -0500

    added add and update functions

--- db/index.js
+++ db/index.js
@@ -4,37 +4,44 @@ class DB {
   // Keeping a reference to the connection on the class in case we need it later
   constructor(connection) {
     this.connection = connection;   // here we reach out to the db so we can do a query
+    this.addDept = this.addDept.bind(this)
+    this.addEmployee = this.addEmployee.bind(this)
+    this.updateEmployee = this.updateEmployee.bind(this)
+    this.addRole = this.addRole.bind(this)
   }
 
-  // Find all employees
-  // THIS IS NOT THE FINAL QUERY. YOU WILL NEED TO MODIFY THIS QUERY SO THAT YOU JOIN 
-  // THE EMPLOYEES WITH THEIR ROLES, SALARIES, DEPARTMENTS, AND MANAGERS
-  // HINT: A TABLE CAN BE JOINED ON ITSELF WITH PROPER TABLE ALIASING
+ findAllDepts(){
+  return this.connection.promise().query("SELECT * FROM department;");
+ } 
+
+ addDept(res) {
+  return this.connection.promise().query(
+    "INSERT INTO department (name) VALUES (?);", [res.department]
+  );
+}
   
-  findAllEmployees() {
-    return this.connection.promise().query(
-      "SELECT * FROM employee;"
-    );
-  }
-
-  // Add more class methods below for all the database operations needed.
-  // Sometimes you may need to pass an id value into a method so it knows 
-  //   how to find the correct record.
-
-
-
-
-
-
-  
-
-
-
-
-  
+findAllEmployees() {
+  return this.connection.promise().query(
+    "SELECT e.id as ID, concat(e.first_name, ' ', e.last_name) AS Name e.manager_id AS Manager, r.title AS Role r.salary AS Salary, d.name as Department FROM employee e INNER JOIN role r ON r.id=role_id INNER JOIN department d ON d.id=department_id;"
+  );
 }
 
+updateEmployee(res){
+  return this.connection.promise().query(
+    "UPDATE employee SET title = (?) WHERE title = (?);" [res.employee, res.title, res.role_id]
+  )
+}
 
+findAllRoles(){
+  return this.connection.promise().query("SELECT * FROM role;");
+}
 
+addRole(res) {
+  return this.connection.promise().query(
+    "INSERT INTO role (title, salary, department_id) VALUES (?,?,?);", [res.title, res.salary, res.department_id]
+  );
+} 
+
+}
 
 module.exports = new DB(connection);
